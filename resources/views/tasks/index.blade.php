21a7839be60cdfa58603dd1fa2540c0e8cd15199
haru.muraoka <haru.3151@icloud.com>
2021-10-25 09:07:26 +0000

    CRUD tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,14 +13,16 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($tasks as $tasks)
+                @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
+    
+    {!! link_to_route('tasks.create', '新タスクの登録', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
