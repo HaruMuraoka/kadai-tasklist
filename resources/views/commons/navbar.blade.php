ec5d9ecb8aee8dcfe2ae7f01bd625307d71d8f5e
haru.muraoka <haru.3151@icloud.com>
2021-10-28 21:34:34 +0000

    user registration2

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,23 +1,19 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Tasklist</a>
+        <a class="navbar-brand" href="/">Microposts</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-        <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                @if (Auth::check())
-                    <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                @else   
-                    {{-- ユーザー登録ページへのリンク --}}
-                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                    {{-- ログインページへのリンク --}}
-                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
-                @endif
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
             </ul>
         </div>
     </nav>
